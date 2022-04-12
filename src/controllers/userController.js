0a5fcbebaad410dd1b9b2145f012fa31392af878
vikash kumar <kumarvik88@gmail.com>
2022-04-12 22:49:35 +0530

    solution book

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,15 @@
-const UserModel= require("../models/userModel")
+const bookModel= require("../models/BookModel")
 
-const createUser= async function (req, res) {
+const bookUser= async function (req, res) {
     let data= req.body
-    let savedData= await UserModel.create(data)
+    let savedData= await bookModel.create(data)
     res.send({msg: savedData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
+const bookUsersData= async function (req, res) {
+    let allUsers= await bookModel.find()
     res.send({msg: allUsers})
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+module.exports.bookUser= bookUser
+module.exports.bookUsersData= bookUsersData
