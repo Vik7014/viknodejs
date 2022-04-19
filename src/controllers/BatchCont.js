852aadb0a19588165696baf2267b2439161d7e23
vikash kumar <kumarvik88@gmail.com>
2022-04-19 16:35:38 +0530

    solution

---
+++ src/controllers/BatchCont.js
@@ -0,0 +1,17 @@
+const batchModel = require("../models/batchModel")
+const developerModel = require("../models/developerModel")
+
+
+const createBatch = async function(req, res){
+    let data = req.body
+    if(! await batchModel.exists(data)){
+        let createdBatch = await batchModel.create(data)
+        res.send({msg: createdBatch})
+    } else{
+        res.send({msg: "This batch already exists"})
+    }
+}
+
+
+
+module.exports.createBatch = createBatch
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const authorController= require("../controllers/authorController")
-const publisherController = require('../controllers/publisherController')
-const bookController= require("../controllers/bookController");
-const { route } = require('express/lib/application');
+
+const batchCont=require("../controllers/BatchCont")
+const developerCont=require("../controllers/developerCont")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createAuthor", authorController.createAuthor  )
 
-router.post('/createPublisher', publisherController.createPublisher)
 
-router.post("/createBook", bookController.createBook  )
-
-router.get('/get-all-books', bookController.fetchbooks)
-
-router.put('/books', bookController.updateBooks)
+router.post("/batches",batchCont.createBatch)
+router.post("/developer",developerCont.createDeveloper)
+router.get("/scholarship-Developers",developerCont.scholarShipDev)
+router.get("/getData",developerCont.developers)
 
 module.exports = router;
